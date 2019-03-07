b0ea582857c02e443378c101cf6a4acede2911a0
unknown <olafkramer2001@gmail.com>
2019-03-07 11:16:45 +0100

    navigatie gemaakt

---
+++ Opdracht 3b/javascript/navigation.js
@@ -0,0 +1,11 @@
+function navigation() {
+    var nav =   "<div class='nav-links-list'> \
+                    <a href='index.html' class='nav-link'>Home</a> \
+                    <a href='contact.html' class='nav-link'>Contact</a> \
+                    <a href='projects.html' class='nav-link'>Projects</a> \
+                </div>";
+
+    document.getElementById('nav').innerHTML = nav;
+}
+
+window.onload = navigation;

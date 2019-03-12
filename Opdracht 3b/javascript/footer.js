3d8a08bc119c86c3978e974262a046f9b8338641
unknown <olafkramer2001@gmail.com>
2019-03-12 15:10:18 +0100

    een gedeelte van de footer gemaakt

--- Opdracht 3b/javascript/footer.js
+++ Opdracht 3b/javascript/footer.js
@@ -1,5 +1,34 @@
 function footer() {
-    var footer = "Hier komt nog een footer";
+    var footer =    "<div class='footerlinks'> \
+                        <h3 class='footerTitle'>Links</h3> \
+                        <ul class='linksList'> \
+                            <li class='listItem'> \
+                                <a class='listItemLink' href='contact.html'>Contact</a> \
+                            </li> \
+                            <li class='listItem'> \
+                                <a class='listItemLink' href='contact.html'>Projects</a> \
+                            </li> \
+                            <li class='listItem'> \
+                                <a class='listItemLink' href='contact.html'>Home</a> \
+                            </li> \
+                        </ul> \
+                    </div> \
+                    <div class='footerMiddle'> \
+                        <form class='searchbar'> \
+                            <input class='search' type='text' placeholder='zoek projecten'></input> \
+                            <button type='submit'>Zoeken</button> \
+                        </form> \
+                        <div class='socialMedia'> \
+                            <a href='#' class='fa fa-facebook'></a> \
+                            <a href='#' class='fa fa-instagram'></a> \
+                            <a href='#' class='fa fa-snapchat-ghost'></a> \
+                            <a href='#' class='fa fa-youtube'></a> \
+                        </div> \
+                    </div> \
+                    <div class='logo'> \
+                        <img src='logo.png' alt='logo'></img> \
+                    </div>";
+
 
     document.getElementById('footer').innerHTML = footer;
 }

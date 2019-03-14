47697994ff64a390ca454c1b854e13d1725948bb
unknown <olafkramer2001@gmail.com>
2019-03-14 21:24:18 +0100

    met javascript iets gemaakt dat je oneindig veel projecten kan toevoegen in grid

--- Opdracht 3b/javascript/makeProjects.js
+++ Opdracht 3b/javascript/makeProjects.js
@@ -2,16 +2,29 @@ function makeProjects(projectName, linkToProjects, description) {
     this.projectName = projectName;
     this.linkToProjects = linkToProjects;
     this.description = description;
-
+    
     this.getProject = function() {
-       document.getElementById('project').innerHTML  = this.projectName + this.linkToProjects + this.description;
+        var project = "<div class='projectBlock'> \
+                           "+ this.projectName +" \
+                        </div>";
+
+        return project;
     }
 }
 
-window.addEventListener("load", function makeevents() { 
-    var project1 = new makeProjects('Appelflap','perzik','mooie beschrijving');
-    var project2 = new makeProjects('baard','jas','in je tas');
+var project1 = new makeProjects('Project1','een link van project 1','beschrijving project1');
+var project2 = new makeProjects('Project2','een link van project 1','beschrijving project2');
+var project3 = new makeProjects('Project3','een link van project 1','beschrijving project3');
+var project4 = new makeProjects('Project4','een link van project 1','beschrijving project4');
+var projectList = '';
+var projectArray = [project1.getProject(), project2.getProject(), project3.getProject(), project4.getProject()];
 
-    project1.getProject();
-    project2.getProject();
+projectArray.forEach(printProjects);
+
+function printProjects(value) {
+    projectList = projectList + value;
+}
+
+window.addEventListener("load", function makeevents() { 
+    document.getElementById('project').innerHTML = projectList;
 })

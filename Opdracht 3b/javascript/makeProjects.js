d8d8fbbe1e8ca0fdb064d6672764808876ac4918
unknown <olafkramer2001@gmail.com>
2019-03-16 21:21:24 +0100

    begin gemaakt aan een modal maken voor een project en de blokken gestyled

--- Opdracht 3b/javascript/makeProjects.js
+++ Opdracht 3b/javascript/makeProjects.js
@@ -4,9 +4,12 @@ function makeProjects(projectName, linkToProjects, description) {
     this.description = description;
     
     this.getProject = function() {
-        var project = "<div class='projectBlock'> \
+        var project =   "<div id='"+this.projectName+"' class='projectBlock'> \
                            "+ this.projectName +" \
                         </div>";
+                        // <div id='modal"+this.projectName+"' class='modal'> \
+                        //     <p>"+ this.description+"</p> \
+                        // </div>;
 
         return project;
     }
@@ -16,8 +19,10 @@ var project1 = new makeProjects('Project1','een link van project 1','beschrijvin
 var project2 = new makeProjects('Project2','een link van project 1','beschrijving project2');
 var project3 = new makeProjects('Project3','een link van project 1','beschrijving project3');
 var project4 = new makeProjects('Project4','een link van project 1','beschrijving project4');
+var project5 = new makeProjects('Project5','een link van project 1','beschrijving project3');
+var project6 = new makeProjects('Project6','een link van project 1','beschrijving project4');
 var projectList = '';
-var projectArray = [project1.getProject(), project2.getProject(), project3.getProject(), project4.getProject()];
+var projectArray = [project1.getProject(), project2.getProject(), project3.getProject(), project4.getProject(), project5.getProject(), project6.getProject()];
 
 projectArray.forEach(printProjects);
 

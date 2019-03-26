a10c91dd1e5bbf4c06bc2c6cbc788c3217c6bead
unknown <olafkramer2001@gmail.com>
2019-03-26 15:25:41 +0100

    opdracht 3 opnieuw toegevoegd

---
+++ Opdracht 3b/javascript/slider.js
@@ -0,0 +1,22 @@
+var number;
+var automatic;
+var image = [];
+image[1] = '../images/code1.jpg';
+image[2] = '../images/code2.jpg';
+image[3] = '../images/code3.jpg';
+image[4] = '../images/code4.jpg';
+
+function next() {
+    number++;
+    if (number >= 5) {
+        number = 1;
+    }
+    document.getElementById('slider').innerHTML = " <h2 class='titleForDocumentOutline'>Image Slider</h2> \
+                                                    <img class='sliderImage' src=" + image[number] + ">";
+}
+
+window.addEventListener("load", function makeevents() { 
+    number = 1;
+    document.getElementById("slider").innerHTML = "<img class='sliderImage' src=" + image[1] + ">";
+    automatic = setInterval(next, 5000);
+})
